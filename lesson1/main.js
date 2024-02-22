b608b4dbcc83ab2415647e4bebe31e4adbbf8a19
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2024-02-22 15:48:08 +0200

    commit1

---
+++ lesson1/main.js
@@ -0,0 +1,50 @@
+/*-------------------------------------------------------- part 1----------------------------------------------------*/
+let hello = 'hello';
+console.log(hello);
+let owu = 'owu';
+console.log(owu);
+let com = 'com';
+console.log(com);
+let ua = 'ua';
+console.log(ua);
+let m = 1;
+console.log(m);
+let n = 10;
+console.log(n);
+let v = -999;
+console.log(v);
+let d = 123;
+console.log(d);
+const PI = 3.14;
+console.log(PI);
+let e = 2.7;
+console.log(e);
+let f = 16;
+console.log(f);
+let g = true;
+console.log(g);
+let h = false;
+console.log(h);
+/*-------------------------------------------------------part 2------------------------------------------------------*/
+
+let firstName = "Natalia";
+let middleName = "Mykolayivna";
+let lastName = "Mykhailova";
+let person = `${lastName} ${firstName} ${middleName}`;
+console.log(person);
+
+/*-----------------------------------------------------part 3---------------------------------------------------------*/
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+/*----------------------------------------------------part 4---------------------------------------------------------*/
+
+let name = prompt("Enter your name:");
+let middle = prompt("Enter your middle name:");
+let age = prompt("Enter your age:");
+console.log("Name:" + " " + name +" "+"Middle name:" + " " + middle +" " +"Age:"+" " + age);
